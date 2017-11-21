97eba9f369fb6636af66bba6d7b12f4f18dc7a02
bredele <olivier.wietrich@gmail.com>
2017-11-21 14:30:05 -0700

    Parse cookie

--- index.js
+++ index.js
@@ -20,29 +20,30 @@ module.exports = {
    * @api public
    */
 
-  serialize(token. options) {
+  serialize(token, options = {}) {
     return cookie.serialize(options.name || 'access_token', token, {
       httpOnly: true,
       secure: true,
       ...options
     })
-  }
+  },
 
 
   /**
    * Create and serialize a cookie session from a JWT token.
    *
    * @param {Object} cookie header
+   * @param {String} name
    * @param {String?} secret
    * return {Promise}
    * @api public
    */
 
 
-  parse(header secret = process.env.JWT_SECRET) {
+  parse(header, name, secret = process.env.JWT_SECRET) {
     const cookies = cookie.parse(header)
     return new Promise((resolve, reject) => {
-      jwt.verify(cookies[name], secret, (err, decoded) => {
+      jwt.verify(cookies[name || 'access_token'], secret, (err, decoded) => {
         if (err) reject(err)
         else resolve(decoded)
       })
--- test/cookie-token.js
+++ test/cookie-token.js
@@ -4,10 +4,29 @@
  */
 
 const test = require('tape')
+const jwt = require('jsonwebtoken')
+const cookie = require('..')
 
-test('this is an example', assert => {
-  assert.plan(1)
-  assert.equal(1 + 2, 3)
+
+test('should serialize a token', assert => {
+  assert.plan(2)
+  const token = jwt.sign({
+    hello: 'world'
+  }, 'thisisasecret')
+  const serialized = cookie.serialize(token)
+  const content = serialized.split(';')[0].split('access_token=')[1]
+  assert.equal(serialized, `access_token=${content}; HttpOnly; Secure`)
+  // token add a iat field
+  assert.deepEqual(jwt.decode(content, 'thisisasecret').hello, 'world')
 })
 
-  
+
+test('should parse a cookie', assert => {
+  assert.plan(1)
+  // serialized version of a {hello: 'world'} payload
+  const serialized = 'access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJoZWxsbyI6IndvcmxkIiwiaWF0IjoxNTExMjk5NjI4fQ.uIMfmX1WSAOMqiuGbfydLk3e4U0vmCtaiSbVaQVs1nU; HttpOnly; Secure'
+  cookie.parse(serialized, 'access_token', 'thisisasecret')
+    .then(payload => {
+      assert.equal(payload.hello, 'world')
+    })
+})
