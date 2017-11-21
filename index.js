9ca9304b73e729eef7daffbe3c11005de109703e
bredele <olivier.wietrich@gmail.com>
2017-11-21 13:34:18 -0700

    Add code documentation

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 
 
 /**
- * This is a simple description.
+ * Create a cookie session from a JWT token.
  *
+ * @param {String} token
+ * @param {String?} secret
  * @api public
  */
 
-module.exports = function () {
+module.exports = function (token, secret = process.env.JWT_SECRET) {
   // do something
 }
